e3fc40988106916b772bd2d7cd126df19592df43
shashank <automation.gaddam@gmail.com>
2024-10-30 21:37:41 -0400

    today

--- src/test/java/testCases/option1.java
+++ src/test/java/testCases/option1.java
@@ -3,10 +3,8 @@ package testCases;
 public class option1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello feature2");
-		System.out.println("hello feature2");
-		System.out.println("hello feature2");
+		
+		System.out.print("k");
 	}
 
 }
